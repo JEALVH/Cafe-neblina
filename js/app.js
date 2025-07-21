827e177b8128e6ded7792a346a26281bde92f402
Jesus Vazquez <jesusavazquezimt@gmail.com>
2025-07-21 16:43:43 -0600

    Diseño de encabezado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const header=documet.querySelector('.header');
+    const menuLinks=document.querySelectorAll('.menu a');
+    const videoBackgrouund=document.querySelector('.video-background');
+    const headerContent=document.querySelector('.header-content');
+
+    window.addEventListener('scroll',function(){
+        if (window.scrollY>100){
+            header.style.height='80px'
+            headerContent.style.backgroundColor='rgba(0,0,0,0.8)'   
+        }else{
+            heade.style.height='100vh';
+            headerContent.style.backgroundColor='rgba(0,0,0,0.3)'
+        }
+    });
+})
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+
+        const targetId=this.getAttribute('href');
+        const targetSection=document.querySelector(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop-(header.classList.conntains('scrolled')? 60:80),       
+        });
+    });
+    
+});
+
 document.getElementById('reservar-btn').addEventListener('click',()=>{
     alert('¡Reserva recibida! Te esperamos.');
 });
